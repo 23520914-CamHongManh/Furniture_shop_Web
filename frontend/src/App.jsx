146e58e451865d6bcd1b90d8e2131f039fba38a1
Long Trịnh <23520891@gm.uit.edu.vn>
2025-11-15 18:27:51 +0700

    Add Product, Cart, Checkout + add_in App.jsx * styles.scss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import { BrowserRouter } from 'react-router-dom'
 import Shop from './components/Shop.jsx'
 import Home from './components/Home.jsx'
 import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Shop from './components/Shop'
+import Product from './components/Product'
+import Cart from './components/Cart'
+import Checkout from './components/Checkout'
 
 function App() {
 
@@ -12,6 +18,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/shop' element={<Shop />}></Route>
+          <Route path='/product' element={<Product/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/checkout' element={<Checkout/>}/>
         </Routes>
       </BrowserRouter>
     </>
