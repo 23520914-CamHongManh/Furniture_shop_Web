1a1f64e8251e6e5f0b086db4d6aeb8ef182f7c3f
Cam Hong Manh <23520914@gm.uit.edu.vn>
2025-11-15 18:56:05 +0700

    preprocess before Designing Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router-dom'
 import Shop from './components/Shop.jsx'
 import Home from './components/Home.jsx'
 import { Routes, Route } from 'react-router-dom'
+import Dashboard from './components/admin/Dashboard.jsx'
+import { AdminRequireAuth } from './components/admin/AdminRequireAuth.jsx'
 
 function App() {
 
@@ -12,6 +14,12 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/shop' element={<Shop />}></Route>
+
+          <Route path='/admin/dashboard' element={
+            <AdminRequireAuth>
+              <Dashboard />
+            </AdminRequireAuth>
+          }></Route>
         </Routes>
       </BrowserRouter>
     </>
