d41c97cf6a7d3a1a6ddef658379dabf59ca8459d
Luanne <23520894@gm.uit.edu.vn>
2025-11-17 14:56:29 +0700

    Add Home Page and Shop Page UI

---
+++ frontend/src/component/common/FeaturedProducts.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import ProductImg1 from '../../assets/images/ten.jpg'
+const FeaturedProducts = () => {
+  return (
+ <section className='section-2 py-5'>
+          <div className='container'>
+                <h2>
+                  Featured Products
+                </h2>
+                <div className='row mt-4'>
+                  <div className='col-md-3 col-6'>
+                    <div className='product card border-0'>
+                      <div className='card-img'>
+                        <img src={ProductImg1} alt='Product1' className='w-100'/>
+                      </div>
+                      <div className='card-body'>
+                        <a href="">
+                          Red-Check-Shirt for Mens
+                        </a>
+                        <div className='price'> 
+                          $50 <span className='text-decoration-line-through'>$100</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                    <div className='col-md-3 col-6'>
+                    <div className='product card border-0'>
+                      <div className='card-img'>
+                        <img src={ProductImg1} alt='Product1' className='w-100'/>
+                      </div>
+                      <div className='card-body'>
+                        <a href="">
+                          Red-Check-Shirt for Mens
+                        </a>
+                        <div className='price'> 
+                          $50 <span className='text-decoration-line-through'>$100</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-md-3 col-6'>
+                    <div className='product card border-0'>
+                      <div className='card-img'>
+                        <img src={ProductImg1} alt='Product1' className='w-100'/>
+                      </div>
+                      <div className='card-body'>
+                        <a href="">
+                          Red-Check-Shirt for Mens
+                        </a>
+                        <div className='price'> 
+                          $50 <span className='text-decoration-line-through'>$100</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-md-3 col-6'>
+                    <div className='product card border-0'>
+                      <div className='card-img'>
+                        <img src={ProductImg1} alt='Product1' className='w-100'/>
+                      </div>
+                      <div className='card-body'>
+                        <a href="">
+                          Red-Check-Shirt for Mens
+                        </a>
+                        <div className='price'> 
+                          $50 <span className='text-decoration-line-through'>$100</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+                </div>
+          </div>
+    </section>
+  )
+}
+
+export default FeaturedProducts
