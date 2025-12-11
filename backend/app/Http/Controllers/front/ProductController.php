76c99a0de98a716bbd1d7868b66fa47bee9ed7ef
Cam Hong Manh <23520914@gm.uit.edu.vn>
2025-12-11 22:41:17 +0700

    fix linking files and loading

--- backend/app/Http/Controllers/front/ProductController.php
+++ backend/app/Http/Controllers/front/ProductController.php
@@ -3,29 +3,30 @@
 namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
-use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\RoomType;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
 
-    public function getProducts(Request $request) {
+    public function getProducts(Request $request)
+    {
 
-        $products = Product::orderBy('created_at','DESC')
-            ->where('status',1);
+        $products = Product::orderBy('created_at', 'DESC')
+            ->where('status', 1);
 
         // Filter Products by category
         if (!empty($request->category)) {
-            $catArray = explode(',',$request->category);
-            $products = $products->whereIn('category_id',$catArray);
+            $catArray = explode(',', $request->category);
+            $products = $products->whereIn('category_id', $catArray);
         }
 
         // Filter Products by brand
-        if (!empty($request->brand)) {
-            $brandArray = explode(',',$request->brand);
-            $products = $products->whereIn('brand_id',$brandArray);
+        if (!empty($request->roomtype)) {
+            $roomtypeArray = explode(',', $request->roomtype);
+            $products = $products->whereIn('room_type_id', $roomtypeArray);
         }
 
         $products = $products->get();
@@ -33,33 +34,36 @@ class ProductController extends Controller
         return response()->json([
             'status' => 200,
             'data' => $products
-        ],200);
+        ], 200);
     }
 
-    public function latestProducts() {
-        $products = Product::orderBy('created_at','DESC')
-                    ->where('status',1)
-                    ->limit(8)
-                    ->get();
+    public function latestProducts()
+    {
+        $products = Product::orderBy('created_at', 'DESC')
+            ->where('status', 1)
+            ->limit(8)
+            ->get();
         return response()->json([
             'status' => 200,
             'data' => $products
-        ],200);
+        ], 200);
     }
 
-    public function featuredProducts() {
-        $products = Product::orderBy('created_at','DESC')
-                    ->where('status',1)
-                    ->where('is_featured', 'yes')
-                    ->limit(8)
-                    ->get();
+    public function featuredProducts()
+    {
+        $products = Product::orderBy('created_at', 'DESC')
+            ->where('status', 1)
+            ->where('is_featured', 'yes')
+            ->limit(8)
+            ->get();
         return response()->json([
             'status' => 200,
             'data' => $products
-        ],200);
+        ], 200);
     }
 
-    public function getCategories() {
+    public function getCategories()
+    {
         $categories = Category::orderBy('name', 'ASC')
             ->where('status', 1)
             ->get();
@@ -67,17 +71,18 @@ class ProductController extends Controller
         return response()->json([
             'status' => 200,
             'data' => $categories
-        ],200);
+        ], 200);
     }
 
-     public function getBrands() {
-        $brands = Brand::orderBy('name', 'ASC')
+    public function getRoomTypes()
+    {
+        $roomtypes = RoomType::orderBy('name', 'ASC')
             ->where('status', 1)
             ->get();
-            
+
         return response()->json([
             'status' => 200,
-            'data' => $brands
-        ],200);
+            'data' => $roomtypes
+        ], 200);
     }
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,13 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/style.scss'
 import { AdminAuthProvider } from './components/context/AdminAuth.jsx';
-import { CartProvider } from './components/context/Cart.jsx';
 import { AuthProvider } from './components/context/Auth.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AdminAuthProvider>
       <AuthProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
+        <App />
       </AuthProvider>
     </AdminAuthProvider>
   </StrictMode>,
